e04f90dc34b1e945878e239d80e4fcb3757ec81e
christopher-witcher <christopher.witcher@gmail.com>
2014-04-25 10:30:27 -0700

    Solution Created for Sibling_Networked

---
+++ Siblings_Networked/Siblings_Networked/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Siblings_Networked.Global" Language="C#" %>
---
+++ Siblings_Networked/Siblings_Networked/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Siblings_Networked.Startup))]
+namespace Siblings_Networked
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
